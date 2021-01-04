64232a2bc36cb9e30f9fa88fcecdf4d4e13220a2
Cris Fandino <sicrnujnuj@gmail.com>
2021-01-04 18:28:46 +0800

    manifest added

--- pages/_layout.js
+++ pages/_layout.js
@@ -7,7 +7,7 @@ const Layout = ({ title, children }) => {
   return (
     <div className="container">
       <SEO title={title} description="Docs">
-        <meta name="theme-color" content="#000000" />
+        <meta name="theme-color" content="#dacb02" />
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/prism-themes@1.5.0/themes/prism-vsc-dark-plus.css"
@@ -16,11 +16,8 @@ const Layout = ({ title, children }) => {
           defer
           src="https://cdn.jsdelivr.net/npm/clipboard@2.0.6/dist/clipboard.min.js"
         ></script>
-        <link
-          rel="shortcut icon"
-          href="https://www.flaticon.com/premium-icon/icons/svg/2581/2581980.svg"
-          type="image/svg"
-        />
+        <link rel="shortcut icon" href="./logo.svg" type="image/svg" />
+        <link rel="manifest" href="manifest.json" />
       </SEO>
       <Sidebar config={sidebarConfig} />
 
