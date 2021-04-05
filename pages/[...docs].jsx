3c52b0b1daa5da8cd238cb0f23d31c14ea695ef7
Cris Fandino <sicrnujnuj@gmail.com>
2021-04-05 09:31:50 +0800

    add a global state for markdown

--- pages/[...docs].jsx
+++ pages/[...docs].jsx
@@ -1,64 +1,72 @@
-import mdParse from '../utils/parseVars'
-import SEO from '../components/SEO'
-import fs from 'fs'
-import generateMdPaths from '../utils/generateMdPaths'
-import { useRouter } from 'next/router'
+import mdParse from "../utils/parseVars";
+import SEO from "../components/SEO";
+import fs from "fs";
+import generateMdPaths from "../utils/generateMdPaths";
+import { useRouter } from "next/router";
 
-import Link from '../components/common/Link'
-import sortLinks, { getNextAndPrevLinks } from '../utils/sortLinks'
-import sidebarConfig from '../sidebar.config'
+import Link from "../components/common/Link";
+import sortLinks, { getNextAndPrevLinks } from "../utils/sortLinks";
+import sidebarConfig from "../sidebar.config";
+import { useGlobalContext } from "../contexts/Global";
+import { useEffect } from "react";
 
-const sortedLinks = sortLinks(sidebarConfig.links)
+const sortedLinks = sortLinks(sidebarConfig.links);
+
 const docs = ({ data, title, description }) => {
-  const { asPath } = useRouter()
-  const prevAndNext = getNextAndPrevLinks(
-    asPath.replace(/#\w+/, ''),
-    sortedLinks,
-  )
-  return (
-    <>
-      <SEO
-        title={title || 'Page Not Found'}
-        description={description || title}
-      />
-      <div>
-        <main
-          dangerouslySetInnerHTML={{
-            __html: data ? data : error,
-          }}
-        />
-        <div className="prev-next-container">
-          {prevAndNext.prev && (
-            <Link href={prevAndNext.prev.href} className="prev-page">
-              {prevAndNext.prev.text}
-            </Link>
-          )}
-          {prevAndNext.next && (
-            <Link href={prevAndNext.next.href} className="next-page">
-              {prevAndNext.next.text}
-            </Link>
-          )}
-        </div>
-      </div>
-    </>
-  )
-}
-export default docs
+   const { asPath } = useRouter();
+   const { dispatch } = useGlobalContext();
+
+   const prevAndNext = getNextAndPrevLinks(
+      asPath.replace(/#\w+/, ""),
+      sortedLinks
+   );
+   useEffect(() => {
+      dispatch("markdownLoaded");
+      return () => dispatch("markdownUnloaded");
+   }, [data]);
+
+   return (
+      <>
+         <SEO title={title} description={description || title} />
+         <div>
+            <main
+               className="markdown"
+               dangerouslySetInnerHTML={{
+                  __html: data ? data : error,
+               }}
+            />
+            <div className="prev-next-container">
+               {prevAndNext.prev && (
+                  <Link href={prevAndNext.prev.href} className="prev-page">
+                     {prevAndNext.prev.text}
+                  </Link>
+               )}
+               {prevAndNext.next && (
+                  <Link href={prevAndNext.next.href} className="next-page">
+                     {prevAndNext.next.text}
+                  </Link>
+               )}
+            </div>
+         </div>
+      </>
+   );
+};
+export default docs;
 
 export async function getStaticPaths() {
-  const data = fs.readdirSync(`docs`)
+   const data = fs.readdirSync(`docs`);
 
-  const paths = generateMdPaths(data, 'docs')
-  return { paths, fallback: false }
+   const paths = generateMdPaths(data, "docs");
+   return { paths, fallback: false };
 }
 export async function getStaticProps({ params }) {
-  const res = await import(`../docs/${params.docs.join('/')}.md`)
-  const { vars, md } = mdParse(res.default)
-  return {
-    props: {
-      title: vars.title || null,
-      description: vars.description || null,
-      data: md,
-    },
-  }
+   const res = await import(`../docs/${params.docs.join("/")}.md`);
+   const { vars, md } = mdParse(res.default);
+   return {
+      props: {
+         title: vars.title || null,
+         description: vars.description || null,
+         data: md,
+      },
+   };
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,49 +1,57 @@
-import mdParse from '../utils/parseVars'
-import SEO from '../components/SEO'
-import indexMD from '../docs/_index.md'
-import { useRouter } from 'next/router'
+import mdParse from "../utils/parseVars";
+import SEO from "../components/SEO";
+import indexMD from "../docs/_index.md";
+import { useRouter } from "next/router";
 
-import Link from '../components/common/Link'
-import sortLinks, { getNextAndPrevLinks } from '../utils/sortLinks'
-import sidebarConfig from '../sidebar.config'
+import Link from "../components/common/Link";
+import sortLinks, { getNextAndPrevLinks } from "../utils/sortLinks";
+import sidebarConfig from "../sidebar.config";
+import { useEffect } from "react";
+import { useGlobalContext } from "../contexts/Global";
 
-const sortedLinks = sortLinks(sidebarConfig.links)
+const sortedLinks = sortLinks(sidebarConfig.links);
 
 const index = () => {
-  const { vars, md } = mdParse(indexMD)
-  const { description, title } = vars
-  const { asPath } = useRouter()
-  const prevAndNext = getNextAndPrevLinks(
-    asPath.replace(/#\w+/, ''),
-    sortedLinks,
-  )
-  return (
-    <>
-      <SEO
-        title={title || 'Page Not Found'}
-        description={description || title}
-      />
-      <div>
-        <main
-          dangerouslySetInnerHTML={{
-            __html: md,
-          }}
-        />
-        <div className="prev-next-container">
-          {prevAndNext.prev && (
-            <Link href={prevAndNext.prev.href} className="prev-page">
-              {prevAndNext.prev.text}
-            </Link>
-          )}
-          {prevAndNext.next && (
-            <Link href={prevAndNext.next.href} className="next-page">
-              {prevAndNext.next.text}
-            </Link>
-          )}
-        </div>
-      </div>
-    </>
-  )
-}
+   const { dispatch } = useGlobalContext();
+   const { vars, md } = mdParse(indexMD);
+   const { description, title } = vars;
+   const { asPath } = useRouter();
+   const prevAndNext = getNextAndPrevLinks(
+      asPath.replace(/#\w+/, ""),
+      sortedLinks
+   );
+   useEffect(() => {
+      dispatch("markdownLoaded");
+      return () => dispatch("markdownUnloaded");
+   }, []);
+   return (
+      <>
+         <SEO
+            title={title || "Page Not Found"}
+            description={description || title}
+         />
+         <div>
+            <main
+               className="markdown"
+               dangerouslySetInnerHTML={{
+                  __html: md,
+               }}
+            />
+            <div className="prev-next-container">
+               {prevAndNext.prev && (
+                  <Link href={prevAndNext.prev.href} className="prev-page">
+                     {prevAndNext.prev.text}
+                  </Link>
+               )}
+               {prevAndNext.next && (
+                  <Link href={prevAndNext.next.href} className="next-page">
+                     {prevAndNext.next.text}
+                  </Link>
+               )}
+            </div>
+         </div>
+      </>
+   );
+};
 
-export default index
+export default index;
